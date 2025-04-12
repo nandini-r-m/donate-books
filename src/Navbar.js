bfd154f086365a880d4bf3da86f28a214e61145b
nandini-r-m <162550211+nandini-r-m@users.noreply.github.com>
2025-04-12 20:05:29 +0530

    updated site

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,7 @@ import'./Navbar.css';
 const Navbar = () => {
     return(
         <nav className = "navbar">
-        <div className="navbar-logo">Books&You</div>
+        <div className="navbar-logo">Books4You</div>
         <ul className="navbar-links">
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
